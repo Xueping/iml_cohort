42f37a1e32370e644234ce6990a22960474e0e0a
xueping <pengxueping@gmail.com>
2017-11-07 16:20:23 +1100

    merge metric learning

--- metric/views.py
+++ metric/views.py
@@ -7,33 +7,58 @@ from django.conf  import settings
 from django.shortcuts import render
 from metric_learn import SDML
 from scipy import io
-from numpy import linalg as LA
+# from numpy import linalg as LA
 
-from clustering.views import clusteringAndPCA
+from clustering.views import clusteringAndTSNE
 import pandas as pd
+import numpy as np
 
 
 # Create your views here.
 def update(request):
     
-    df_label = io.mmread(os.path.join(settings.BASE_DIR, 'data/outcome_labels.mtx')).todense()
+    df_label = pd.read_csv(os.path.join(settings.BASE_DIR, 'data/outcome_labels.csv'))
     
-#     print df_label.
+    df_data = pd.read_csv(os.path.join(settings.BASE_DIR, 'data/features_rep.csv'))
     
-    df_data = pd.read_csv(os.path.join(settings.BASE_DIR, 'data/features_rep.csv')).as_matrix()
+    #remove dupicate
+    rowIDLIst = pd.concat([df_label.id1,df_label.id2],axis = 0).unique().tolist()
+    print rowIDLIst
+
+    cmatrix = np.zeros([len(rowIDLIst),len(rowIDLIst)])
+#     print df_label
+# #     next(df_label.iterrows())
+    for lbl in df_label.as_matrix():
+        print lbl
+        cmatrix[rowIDLIst.index(lbl[0])][rowIDLIst.index(lbl[1])] = int(lbl[2])
+        cmatrix[rowIDLIst.index(lbl[1])][rowIDLIst.index(lbl[0])] = int(lbl[2])
+   
+    trainedData = []
     
-#     al_selection = request.session['clustering']
-#     num_clustering = request.session['num_cluster'] 
+    for rid in rowIDLIst:
+        row = df_data.iloc[[rid]]
+        trainedData.append(row)
+        
+    print trainedData.dtpye
+    print cmatrix.dtpye
     
-    print LA.cond(df_label)
-    print df_label
-    print df_label.shape
-    print LA.cond(df_data)
-    print df_data.shape
-    metric = SDML().fit(df_data, df_label)
+    metric = SDML().fit(trainedData, cmatrix)  
+    
+    newData = metric.transform(df_data) 
+    
+    al_selection = request.session['clustering']
+    num_clustering = request.session['num_cluster'] 
+
+    
+#     print LA.cond(df_label)
+#     print df_label
+#     print df_label.shape
+#     print LA.cond(df_data)
+#     print df_data.shape
+#     metric = SDML().fit(df_data, df_label)
 #     .transform(df_data)
     
-#     clusteringAndPCA(metric,al_selection,num_clustering)
+    clusteringAndTSNE(newData,al_selection,num_clustering)
         # context is a dict of html code, containing three types of features representation
     content = {'Title': "Step 7: Clustering Visualization",
                "listId":"li7"} 
